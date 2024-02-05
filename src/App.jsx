ddc60a983e48a80ef4e98adeeb48aa56d4dd66d8
AntonCharnichenka <antt0n.chern1chenk0@gmail.com>
2024-02-05 09:12:45 +0100

    inline handlesrs

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,139 @@
 import * as React from 'react';
 
-function App() {
-  const [text, setText] = React.useState('Some text ...');
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
 
-  function handleOnChange(event) {
-    setText(event.target.value);
-  }
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
 
-  const ref = (node) => {
-    console.log('REF CALLBACK CALLED')
-    
-    if (!node) {
-      console.log('RETURNING NOTHING FROM REF CALLBACK')
-      return
-    };
+  return [value, setValue];
+};
 
-    console.log('REF CALLBACK CALLED WITH NODE', node)
-    const { width } = node.getBoundingClientRect();
+const App = () => {
+  const initialStories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: 'Reexample',
+      url: 'https://redux.js.org/',
+      author: 'Antoshka',
+      num_comments: 3,
+      points: 7,
+      objectID: 2,
+    },
+  ]
 
-    if (width >= 150) {
-      node.style.color = 'red';
-    } else {
-      node.style.color = 'blue';
-    }
+  const [stories, setStories] = React.useState(initialStories);
+
+  const removeObjectFromStories = (objectId) => {
+    const newStories = stories.filter(
+      (story) => story.objectID !== objectId
+    );
+    setStories(newStories);
   };
+
+  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
-      <input type="text" value={text} onChange={handleOnChange} />
-      <div>
-        <span ref={ref}>{text}</span>
-      </div>
+      <h1>My Hacker Stories</h1>
+
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
+
+      <hr />
+
+      <List list={searchedStories} onRemove={removeObjectFromStories}/>
     </div>
   );
-}
+};
+
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children,
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
+
+const List = ({ list, onRemove }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} onRemove={onRemove} />
+    ))}
+  </ul>
+);
+
+const Item = ({ item, onRemove }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span><RemoveButton item={item} onRemove={onRemove}/></span>
+  </li>
+);
+
+const RemoveButton = ({ item, onRemove }) => (
+    <button type="button" onClick={() => onRemove(item.objectID)}>
+      Dismiss
+    </button>
+  );
 
 export default App;
-
-// create a class with a constructor with parameter 'number' and method to print it
-class Number {
-  constructor(number) {
-    this.number = number;
-  }
-
-  print() {
-    console.log(this.number);
-  }
-}
-
-// create instance of this class
-const number = new Number(5);
