f7829b623239c94ebd6d86f41f6fafde85dea06d
AntonCharnichenka <antt0n.chern1chenk0@gmail.com>
2024-03-06 17:02:02 +0100

    reintroduce simple reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
 import * as React from 'react';
 
-const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const getAsyncStories = () =>
+  new Promise((resolve) =>
+    setTimeout(
+      () => resolve({ data: { stories: initialStories } }),
+      2000
+    )
+  );
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
-    case 'STORIES_FETCH_INIT':
-      return {
-        ...state,
-        isLoading: true,
-        isError: false,
-      };
-    case 'STORIES_FETCH_SUCCESS':
-      return {
-        ...state,
-        isLoading: false,
-        isError: false,
-        data: action.payload,
-      };
-    case 'STORIES_FETCH_FAILURE':
-      return {
-        ...state,
-        isLoading: false,
-        isError: true,
-      };
+    case 'SET_STORIES':
+      return action.payload;
     case 'REMOVE_STORY':
-      return {
-        ...state,
-        data: state.data.filter(
-          (story) => action.payload.objectID !== story.objectID
-        ),
-      };
+      return state.filter(
+        (story) => action.payload.objectID !== story.objectID
+      );
     default:
       throw new Error();
   }
@@ -55,26 +60,24 @@ const App = () => {
 
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
-    { data: [], isLoading: false, isError: false }
+    []
   );
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
-  React.useEffect(
-    () => {
-      if (!searchTerm.trim()) return;
+  React.useEffect(() => {
+    setIsLoading(true);
 
-      dispatchStories({ type: 'STORIES_FETCH_INIT' });
-      fetch(
-        `${API_ENDPOINT}${searchTerm}`
-      ).then(
-        response => response.json()
-      ).then(
-        result => dispatchStories({type: 'STORIES_FETCH_SUCCESS', payload: result.hits})
-      ).catch(
-        () => dispatchStories({type: 'STORIES_FETCH_FAILURE'})
-      )
-    },
-    [searchTerm]
-  );
+    getAsyncStories()
+      .then((result) => {
+        dispatchStories({
+          type: 'SET_STORIES',
+          payload: result.data.stories,
+        });
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
+  }, []);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -86,6 +89,10 @@ const App = () => {
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
@@ -102,13 +109,13 @@ const App = () => {
 
       <hr />
 
-      {stories.isError && <p>Something went wrong ...</p>}
+      {isError && <p>Something went wrong ...</p>}
 
-      {stories.isLoading ? (
+      {isLoading ? (
         <p>Loading ...</p>
       ) : (
         <List
-          list={stories.data}
+          list={searchedStories}
           onRemoveItem={handleRemoveStory}
         />
       )}
@@ -164,9 +171,9 @@ const Item = ({ item, onRemoveItem }) => (
     <span>
       <a href={item.url}>{item.title}</a>
     </span>
-    <span> {item.author}</span>
-    <span> {item.num_comments}</span>
-    <span> {item.points} </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
     <span>
       <button type="button" onClick={() => onRemoveItem(item)}>
         Dismiss
